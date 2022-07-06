fa8a7d3dd4e44438cb190a03a788eeaa095ef357
Panlabek <piplup654@protonmail.com>
2022-07-06 18:06:51 +0200

    Added unittests for rest of the helper functions

--- test_math_db.py
+++ test_math_db.py
@@ -53,7 +53,7 @@ def test_update_ex_status_and_df():
     ex.ex_status = status
     ex.date_finished = df
     assert ex_db.fetch_all_exercises()[0] == ex.get_ex_as_tuple()
-def test_marking_status_as_done():
+def test_marking_status_as_done_for_every_ex():
     ex_db = create_dummy_db()
     first_ex = 0
     last_ex = randint(7,100)
@@ -120,3 +120,14 @@ def test_fetch_all_exercises():
     chapter = randint(0,10)
     ex_db.create_dummy_exercises_for_chapter_given_range(first_exercise=first_ex, last_exercise=last_ex, chapter=chapter)
     assert len(ex_db.fetch_all_exercises()) == (last_ex+1)
+def test_get_todays_date():
+    tdate = datetime.datetime.strftime(datetime.datetime.now(), "%Y-%m-%d")
+    ex_db = create_dummy_db()
+    assert ex_db.get_todays_date() == tdate
+def test_count_ex_list():
+    counter = 0
+    ex_db = create_dummy_db()
+    lister = [x for x in range (20)]
+    for _ in lister:
+        counter += 1
+    assert ex_db.count_exercise_list(lister) == counter
