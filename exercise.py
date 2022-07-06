ddd13be66ad3391df69f89081f2c88afbf629f46
Panlabek <piplup654@protonmail.com>
2022-07-06 15:42:50 +0200

    Defaulted every arg to None for the sake of comfort. Added __str__ method for convenience in unittesting.

--- exercise.py
+++ exercise.py
@@ -1,6 +1,8 @@
 class Exercise():
-    def __init__(self, ex_number, chapter, ex_status,date_finished):
+    def __init__(self, ex_number=None, chapter=None, ex_status=None,date_finished=None):
         self.ex_number = ex_number
         self.chapter = chapter
-        self.date_finished = date_finished
         self.ex_status = ex_status
+        self.date_finished = date_finished
+    def __str__(self):
+        return f"ex_number:{self.ex_number}, chapter:{self.chapter}, ex_status:{self.ex_status}, date_finished:{self.date_finished}"
