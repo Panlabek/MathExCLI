17c4819a5f04e6d693a6af73d3d42b278c1af099
Panlabek <piplup654@protonmail.com>
2022-07-06 16:23:36 +0200

    Refactored archival funcs to helper function for exercise and db creation. Added tests for removing exercise from db.

--- test_math_db.py
+++ test_math_db.py
@@ -1,24 +1,26 @@
 from math_db import Math_db
-from exercise import Exercise
-
+from testing_helpers import create_dummy_db, create_dummy_exercise, create_rand_exercise, create_exercise_with_input
 def test_exercise_class_test1():
     ex_number = 2
-    ex1 = Exercise(ex_number=ex_number)
+    ex1 = create_exercise_with_input(ex_number, 0, "")
     assert ex1.ex_number == ex_number
 def test_exercise_class_test2():
     chapter = 3
-    ex1 = Exercise(chapter=chapter)
+    ex1 = create_exercise_with_input(0, chapter, "")
     assert ex1.chapter == chapter
 def test_exercise_class_test3():
     status = "DONE"
-    ex1 = Exercise(ex_status=status)
+    ex1 = create_exercise_with_input(0, 0, status)
     assert ex1.ex_status == status
-def test_insert_test():
-    ex = Exercise()
+def test_insert_exercise():
+    ex = create_dummy_exercise()
     ex_db = Math_db(":memory:")
     ex_db.insert_exercise(ex)
-    ret = ex_db.fetch_all_exercises()
-    assert ret[0][0] == ex.ex_number
-    assert ret[0][1] == ex.chapter
-    assert ret[0][2] == ex.ex_status
-    assert ret[0][3] == ex.date_finished
+    assert ex_db.fetch_all_exercises()[0] == ex.get_ex_as_tuple()
+def test_remove_exercise():
+    ex = create_rand_exercise()
+    ex_db = create_dummy_db()
+    ex_db.insert_exercise(ex)
+    assert ex_db.fetch_all_exercises()[0] == ex.get_ex_as_tuple()
+    ex_db.remove_exercise(ex)
+    assert ex_db.fetch_all_exercises()[0] == ex.get_ex_as_tuple()
